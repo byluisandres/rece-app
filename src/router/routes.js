f63912245cd585187f57ca7df5e3096202fe8a5d
Luis Andrés Bolaños Yapo <luisandresbolanosdev@gmail.com>
2022-10-15 17:16:24 +0200

    ordenar rutas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,22 @@
 const routes = [
   {
     path: "/",
+    redirect: { name: "login" },
+  },
+  {
+    path: "/auth/login",
     name: "login",
     component: () => import("../pages/auth/login.vue"),
   },
   {
-    path: "/register",
+    path: "/auth/register",
     name: "register",
     component: () => import("../pages/auth/register.vue"),
+  },
+  {
+    path: "/auth/recover-password",
+    name: "recover-password",
+    component: () => import("../pages/auth/recover-password.vue"),
   },
 ];
 
