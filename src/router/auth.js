48e8861292649fb65ffecca90ab4f1c97f95902c
Luis Andrés Bolaños Yapo <luisandresbolanosdev@gmail.com>
2022-10-16 16:20:29 +0200

    reestructuracion de las rutas

---
+++ src/router/auth.js
@@ -0,0 +1,23 @@
+const auth = [
+  {
+    path: "/",
+    redirect: { name: "login" },
+  },
+  {
+    path: "/auth/login",
+    name: "login",
+    component: () => import("../pages/auth/login.vue"),
+  },
+  {
+    path: "/auth/register",
+    name: "register",
+    component: () => import("../pages/auth/register.vue"),
+  },
+  {
+    path: "/auth/recover-password",
+    name: "recover-password",
+    component: () => import("../pages/auth/recover-password.vue"),
+  },
+];
+
+export default auth;
