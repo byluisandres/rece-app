ff7383464046648df6af0c0a378af26aa7f9ed90
Luis Andrés Bolaños Yapo <luisandresbolanosdev@gmail.com>
2022-10-16 16:22:06 +0200

    index calendar

---
+++ src/pages/app/calendar/index.vue
@@ -0,0 +1,33 @@
+<template lang="">
+        <FullCalendar :options="calendarOptions" />
+</template>
+<script>
+import "@fullcalendar/core/vdom"; // solves problem with Vite
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import esLocale from "@fullcalendar/core/locales/es";
+
+export default {
+  name: "calendar",
+  components: {
+    FullCalendar,
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+        locale: esLocale,
+        headerToolbar: {
+          start: "title", // will normally be on the left. if RTL, will be on the right
+          center: "",
+          end: "prev,next",
+        },
+        initialView: "timeGridWeek",
+      },
+    };
+  },
+};
+</script>
+<style lang=""></style>
