bb0528b3155b2801a1355d57033de0b3a7d19a8b
Luis Andrés Bolaños Yapo <luisandresbolanosdev@gmail.com>
2022-10-16 16:21:33 +0200

    navegacion por tabbar

---
+++ src/pages/app/index.vue
@@ -0,0 +1,30 @@
+<template lang="">
+  <van-nav-bar title="Recetas" @click-right="onClickRight">
+    <template #right>
+      <van-icon name="plus" />
+    </template>
+  </van-nav-bar>
+  <router-view></router-view>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="wap-home-o" to="/recipes">Recipes</van-tabbar-item>
+    <van-tabbar-item icon="calendar-o" to="/calendar">Calendar</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">Configuración</van-tabbar-item>
+    <van-tabbar-item icon="user-circle-o">Perfil</van-tabbar-item>
+  </van-tabbar>
+</template>
+<script>
+export default {
+  name: "app-index",
+  data() {
+    return {
+      active: 0,
+    };
+  },
+  methods: {
+    onClickRight() {
+      this.$router.push({ name: "recipes-new" });
+    },
+  },
+};
+</script>
+<style lang=""></style>
