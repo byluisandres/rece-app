92c9b94235cefa10e2d3a2e7a4a7956aa6f4662d
Luis Andrés Bolaños Yapo <luisandresbolanosdev@gmail.com>
2022-10-16 18:06:20 +0200

    nuevas rutas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,15 @@
 import auth from "./auth";
 import recipes from "./recipes";
 import calendar from "./calendar";
+import configuration from "./configuration";
+import profile from "./profile";
 
 const routes = [
   ...auth,
   {
     path: "/app",
     component: () => import("../pages/app/index.vue"),
-    children: [...recipes, ...calendar],
+    children: [...recipes, ...calendar, ...configuration, ...profile],
   },
 ];
 
