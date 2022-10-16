a2375e06cda85b15c515d2e054c25c637eaa11ce
Luis Andrés Bolaños Yapo <luisandresbolanosdev@gmail.com>
2022-10-16 18:06:35 +0200

    tab

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -8,8 +8,8 @@
   <van-tabbar v-model="active">
     <van-tabbar-item icon="wap-home-o" to="/recipes">Recipes</van-tabbar-item>
     <van-tabbar-item icon="calendar-o" to="/calendar">Calendar</van-tabbar-item>
-    <van-tabbar-item icon="setting-o">Configuración</van-tabbar-item>
-    <van-tabbar-item icon="user-circle-o">Perfil</van-tabbar-item>
+    <van-tabbar-item icon="setting-o" to="/configuration">Configuración</van-tabbar-item>
+    <van-tabbar-item icon="user-circle-o" to="/profile">Perfil</van-tabbar-item>
   </van-tabbar>
 </template>
 <script>
